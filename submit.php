7acc8fb8fa3d7a43966a5ae34835a8e43d17bed1
Aidas Jacevičius <144323374+AidasJcv@users.noreply.github.com>
2023-11-29 21:26:11 +0200

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,28 @@
+<?php
+
+	$newURL = "/order-failed";
+
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		
+		if (isset($_POST['ram'], $_POST['cpu'], $_POST['vcpu'], $_POST['storage'])) {
+			
+			$newURL = "/thank-you";
+			
+			$ram = $_POST['ram'];
+			$cpu = $_POST['cpu'];
+			$vcpu = $_POST['vcpu'];
+			$storage = $_POST['storage'];
+
+			echo "RAM: " . $ram . "MB<br>";
+			echo "CPU: " . $cpu . " Cores<br>";
+			echo "VCPU: " . $vcpu . " Cores<br>";
+			echo "SSD: " . $storage . "GB<br>";
+		} else {
+			echo "POST variables are not set.";
+		}
+	} else {
+		echo "This script only accepts POST requests.";
+	}
+	
+	header('Location: '.$newURL);
+?>
