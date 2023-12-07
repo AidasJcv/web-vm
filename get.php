0f494339a494420fd52c968707460b2ddb73e59c
Aidas Jacevičius <144323374+AidasJcv@users.noreply.github.com>
2023-12-07 20:31:42 +0200

    Update get.php

--- get.php
+++ get.php
@@ -29,11 +29,11 @@
 				<form action="/scripts/submit.php" method="POST" id="getserver">
 					<div>
 						<p style="text-align: left;">RAM: <span id="ram_display"></span>MB</p>
-						<input name="ram" id="ram_slider" style="width: 100%" type="range" min="128" max="1024" value="256" step="64">
+						<input name="ram" id="ram_slider" style="width: 100%" type="range" min="256" max="1024" value="384" step="64">
 					</div>
 					<div>
-						<p style="text-align: left;">CPU: <span id="cpu_display"></span> Cores</p>
-						<input name="cpu" id="cpu_slider" style="width: 100%" type="range" min="1" max="2" value="1" step="1">
+						<p style="text-align: left;">CPU: <span id="cpu_display"></span>VCores</p>
+						<input name="cpu" id="cpu_slider" style="width: 100%" type="range" min="0.1" max="0.3" value="1" step="1">
 					</div>
 					<input type="submit" value="Get Server" form="getserver">
 				</form>
