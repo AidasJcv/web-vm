97821241780e301d11d9a13f73c7b42c424917e9
Aidas Jacevičius <144323374+AidasJcv@users.noreply.github.com>
2023-12-07 13:07:50 +0200

    Update submit.php

--- scripts/submit.php
+++ scripts/submit.php
@@ -3,26 +3,59 @@
 	$newURL = "/order-failed.php";
 
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		
-		if (isset($_POST['ram'], $_POST['cpu'], $_POST['vcpu'], $_POST['storage'])) {
-			
-			$newURL = "/thank-you";
-			
+
+		if (isset($_POST['ram'], $_POST['cpu'])) {
+
 			$ram = $_POST['ram'];
 			$cpu = $_POST['cpu'];
-			$vcpu = $_POST['vcpu'];
-			$storage = $_POST['storage'];
 
-			echo "RAM: " . $ram . "MB<br>";
-			echo "CPU: " . $cpu . " Cores<br>";
-			echo "VCPU: " . $vcpu . " Cores<br>";
-			echo "SSD: " . $storage . "GB<br>";
-		} else {
-			echo "POST variables are not set.";
+
+			$wshost = 'localhost';
+			$wsusername = 'webserver';
+			$wspassword = 'admin';
+			$wsdatabase = 'sessions';
+
+			$ipfile = fopen("../databaseIP.txt", "r") or die("Unable to open file!");
+			$remote_database_host = fgets($ipfile);
+			fclose($ipfile);
+
+			$remote_database_host = str_replace(" ", "", $remote_database_host);
+			$remote_database_host = str_replace("\n", "", $remote_database_host);
+
+			$dbhost = $remote_database_host;
+			$dbusername = "admin";
+			$dbpassword = "admin";
+			$dbdatabase = "users";
+
+			$mysqli = new mysqli($wshost, $wsusername, $wspassword, $wsdatabase);
+			$dbmysqli = new mysqli($dbhost, $dbusername, $dbpassword, $dbdatabase);
+			
+			if ($mysqli->connect_error) {
+				echo 'Connection failed: ' . $mysqli->connect_error;
+				exit();
+			}
+
+			if ($dbmysqli->connect_error) {
+				echo 'Connection failed: ' . $dbmysqli->connect_error;
+				exit();
+			}
+			
+			$cookie_value = $_COOKIE["session_id"];
+			$cookie_value = $mysqli->real_escape_string($cookie_value);
+			$query = "select user_id from sessions_data where session_id='$cookie_value'";
+			$result = $mysqli->query($query);
+			
+			if ($result && $result->num_rows > 0) {
+				$row = $result->fetch_assoc();
+				
+				$query = "insert into vminfo (owner_id, cpu, ram) values (" . $row["user_id"] . ", $cpu, $ram, '')";
+				$result = $dbmysqli->query($query);
+				
+				$newURL = "/thank-you";
+			}
 		}
-	} else {
-		echo "This script only accepts POST requests.";
 	}
-	
+
 	header('Location: '.$newURL);
+
 ?>
