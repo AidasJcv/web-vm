684388d5c5c7eeda815eae9e6f949e49072f2f13
Aidas Jacevičius <144323374+AidasJcv@users.noreply.github.com>
2023-12-12 11:38:26 +0200

    Update panel.php

--- panel.php
+++ panel.php
@@ -26,7 +26,62 @@
 		<div id="inner" style="text-align: center">
 			<h2>Active virtual machines:</h2>
 			<?php
+
+				$wshost = 'localhost';
+				$wsusername = 'webserver';
+				$wspassword = 'admin';
+				$wsdatabase = 'sessions';
+	
+				$ipfile = fopen("../databaseIP.txt", "r") or die("Unable to open file!");
+				$remote_database_host = fgets($ipfile);
+				fclose($ipfile);
+	
+				$remote_database_host = str_replace(" ", "", $remote_database_host);
+				$remote_database_host = str_replace("\n", "", $remote_database_host);
+	
+				$dbhost = $remote_database_host;
+				$dbusername = "admin";
+				$dbpassword = "admin";
+				$dbdatabase = "users";
+	
+				$mysqli = new mysqli($wshost, $wsusername, $wspassword, $wsdatabase);
+				$dbmysqli = new mysqli($dbhost, $dbusername, $dbpassword, $dbdatabase);
 				
+				if ($mysqli->connect_error) {
+					echo 'Connection failed: ' . $mysqli->connect_error;
+					exit();
+				}
+	
+				if ($dbmysqli->connect_error) {
+					echo 'Connection failed: ' . $dbmysqli->connect_error;
+					exit();
+				}
+				
+				$cookie_value = $_COOKIE["session_id"];
+				$cookie_value = $mysqli->real_escape_string($cookie_value);
+				$query = "select user_id from sessions_data where session_id='$cookie_value'";
+				$result = $mysqli->query($query);
+				
+				if ($result && $result->num_rows > 0) {
+					$row = $result->fetch_assoc();
+					
+					$query = "select vm_id, connect_info from users.vminfo where owner_id=$user_id";
+					$result = $dbmysqli->query($query);
+					
+					if ($result && $result->num_rows > 0) {
+						while ($row = $dbmysqli->fetch_assoc()) {
+
+							echo "VM ID: " . $row["vm_id"] . ", SSH CONNECTION INFO: " . $row["connection_info"] . "<br>";
+							
+						}
+
+					}
+				} else {
+
+					
+					
+				}
+
 			?>
 		</div>
 	</div>
